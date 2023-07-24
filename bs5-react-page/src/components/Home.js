2773e2a0cebf59c984caf89296a79d514e2de84e
kevlkq <luakerquan@gmail.com>
2023-07-24 11:18:02 +0800

    changed home to hompage

--- bs5-react-page/src/components/Home.js
+++ bs5-react-page/src/components/Home.js
@@ -41,18 +41,14 @@ const Home = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home" >Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <Nav.Link className="font-weight-dark" href="#home">HomePage</Nav.Link>
+              <Nav.Link className="font-weight-dark" href="#link">Link</Nav.Link>
+                <NavDropdown title="Navigate to" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Training</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
-                    Another action
+                    Testing
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Predictions</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
--- bs5-react-page/src/components/TrainPage.js
+++ bs5-react-page/src/components/TrainPage.js
@@ -6,6 +6,16 @@ import { motion } from 'framer-motion';
 import stylesTrain from './TrainButtons.module.css';
 import DatasetPreviewModal from './DatasetPreviewModal';
 import CsvHeadersModal from './CsvHeadersModal';
+import Button from 'react-bootstrap/Button';
+import Card  from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 // import DropColModal from './DropColModal'; 
 
 const TrainPage = () => {
@@ -147,6 +157,38 @@ const TrainPage = () => {
 
   return (
     <Transitions>
+      <header >
+        <Navbar bg="warning" data-bs-theme="light">
+          <Container>
+            <Navbar.Brand href="#home">
+            <img
+                alt=""
+                src="https://www.startupsg.gov.sg/api/v0/profile-images/2447/image/avatar"
+                width="75"
+                height="75"
+                className="d-inline-block align-center"
+              />{' '}Oak Consulting</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home" >Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     <div className={stylesTrain.container}>
       <div className={stylesTrain.modelsContainer}>
         <form onSubmit={handleSubmit}>
